03ac36ec172f66e34d87f9d69bc4acfe0fcafcee
Ed Whyatt <lizardxyz@googlemail.com>
2022-08-16 16:01:44 +0000

    Add adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav initialization from Materialize
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization from Materialize
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18in: {
+        done: "select"
+      }
+    });
+
+    // select initialization from Materialize
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
