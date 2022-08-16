60678ed587494b401ae7c55d3bd31e086da77522
Ed Whyatt <lizardxyz@googlemail.com>
2022-08-16 11:50:49 +0000

    Add home page base and template inheritance

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidenav initialization from Materialize
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
